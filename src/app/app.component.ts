2b7692dbf57b578a6496382f9aa5bb75eb66f433
adiakogiannis <alexios.diakogiannis@soprasteria.com>
2021-05-03 13:07:37 +0200

    Finished static pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,7 +16,11 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private oneSignal: OneSignal,
+    private statusBar: StatusBar,
+    private nav: NavController,
+    private loading: LoadingController,
+    private alertCtrl: AlertController
   ) {
     this.initializeApp();
   }
@@ -22,6 +29,56 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      if (this.platform.is('cordova')) {
+        this.setupPush();
+      }
     });
   }
+
+  setupPush() {
+    // I recommend to put these into your environment.ts
+    this.oneSignal.startInit('823b73f5-69dc-4c80-877e-9c4ce53de108', 'AIzaSyAflfS15s6YTm0Xno_3PhkTYKmCmjPh1NY');
+
+    this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.None);
+
+    // Notifcation was received in general
+    this.oneSignal.handleNotificationReceived().subscribe(data => {
+      let msg = data.payload.body;
+      let title = data.payload.title;
+      let additionalData = data.payload.additionalData;
+      this.showAlert(title, msg, additionalData.task);
+
+
+      console.log(data);
+    });
+
+    // Notification was really clicked/opened
+    this.oneSignal.handleNotificationOpened().subscribe(data => {
+      // Just a note that the data is a different place here!
+      let additionalData = data.notification.payload.additionalData;
+      this.showAlert('Notification opened', 'You already read this before', additionalData.task);
+    });
+
+    this.oneSignal.endInit();
+  }
+
+  async showAlert(title, msg, task) {
+    const alert = await this.alertCtrl.create({
+      header: title,
+      subHeader: msg,
+      buttons: [
+        {
+          text: `Action: ${task}`,
+          handler: () => {
+            // E.g: Navigate to a specific screen
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+
 }
+
+
